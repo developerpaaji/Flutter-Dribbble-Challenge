e04742f2c03479fa1b94d85e2ddde9d28886db4e
Bhavneet singh <singhbhavneetdeveloper@gmail.com>
2019-05-20 06:31:35 +0530

    Challenge 4

---
+++ lib/challenge3/widgets/draggable_card.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DraggableCard extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _DraggableCardState();
+
+}
+
+class _DraggableCardState extends State<DraggableCard> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
